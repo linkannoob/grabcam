e3c87376c4f8183f56c6356586753b77be96c9c3
linkannoob <53121929+linkannoob@users.noreply.github.com>
2020-01-29 15:00:45 +0530

    Update grabcam.sh
    Hi

--- grabcam.sh
+++ grabcam.sh
@@ -28,7 +28,7 @@ echo '
                                                                                
 echo " "
 printf "      \e[1;77m v1.0 coded by github.com/thelinuxchoice/saycheese\e[0m \n"
-printf "          \e[1;77m v1.1 This reborn script by { Noob Hackers }\e[0m \n"
+printf "          \e[1;77m v1.1 This reborn script by { WhiteHat Hacker }\e[0m \n"
 
 printf "\n"
 
